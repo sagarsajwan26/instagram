7d901339ae28feb24801a5826aa8169bf5fc2a3a
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-08-07 10:33:51 +0530

    message

--- server/src/app.js
+++ server/src/app.js
@@ -18,8 +18,8 @@ app.use(cors({
 }));
 
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser());
 
